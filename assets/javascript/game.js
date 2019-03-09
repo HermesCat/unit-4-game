9d8d3a36862a2336406b6ab8b99e42648de2841e
HermesCat <jppettis@comcast.net>
2019-03-09 00:30:38 -0500

    stufffff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -92,11 +92,3 @@ function reset() {
 console.log("current total is " + total);
 
 
-//iphone zoom problems
-$(document).click(function(event) {
-  element = document.elementFromPoint(event.clientX, event.clientY);
-  if(document.getElementById("div_excluded_from_doubletap").contains(element)) {
-      event.preventDefault();
-      clickFunction(element);
-  }
-});
