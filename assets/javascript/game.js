b64f84ef08ce8d531550d472d2633140bf5f1aa7
HermesCat <jppettis@comcast.net>
2019-03-09 00:15:28 -0500

    stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -90,3 +90,15 @@ function reset() {
 }
 
 console.log("current total is " + total);
+
+
+//iphone zoom problems
+function AllowZoom(flag) {
+  if (flag == true) {
+    $('head meta[name=viewport]').remove();
+    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10.0, minimum-scale=1, user-scalable=1" />');
+  } else {
+    $('head meta[name=viewport]').remove();
+    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />');              
+  }
+}
